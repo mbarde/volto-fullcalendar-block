bc560185dc06decd88e21a8bbac198e464c64473
Matthias Barde <matthiasbarde@gmx.de>
2021-07-08 15:32:15 +0200

    fix SSR bug

--- src/components/manage/Blocks/FullCalendar/View.jsx
+++ src/components/manage/Blocks/FullCalendar/View.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import iCalendarPlugin from '@fullcalendar/icalendar';
 
 
 const FullCalendarBlockView = (props) => {
+  /* server-side rendering with FullCalendar does not work here,
+     so we need to render after client-side hydration - as described here:
+     https://gist.github.com/gaearon/e7d97cdf38a2907924ea12e4ebdf3c85#option-2-lazily-show-component-with-uselayouteffect
+  */
+  const [showCalendar, setShowCalendar] = useState(false);
+
+  useEffect(() => {
+    setShowCalendar(true);
+  }, []);
+
+  if (!showCalendar) {
+    /* placeholder? */
+    return null;
+  }
+
+  const { data } = props;
+  const events = data.calendar_url ? {
+    url: data.calendar_url,
+    format: 'ics'
+  } : {};
+
   return (
     <FullCalendar
-      plugins={[dayGridPlugin]}
+      plugins={[dayGridPlugin, iCalendarPlugin]}
       initialView="dayGridMonth"
+      events={events}
     />
   );
 };
