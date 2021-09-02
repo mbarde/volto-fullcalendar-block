165cb46a5b7826446a1215bc5344bd4b45f8163d
Matthias Barde <matthiasbarde@gmx.de>
2021-09-02 13:41:25 +0200

    store retrieved events in state to fix #2

--- src/components/manage/Blocks/FullCalendar/View.jsx
+++ src/components/manage/Blocks/FullCalendar/View.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import FullCalendar from '@fullcalendar/react';
-import { Loader } from 'semantic-ui-react'
+import { Loader } from 'semantic-ui-react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import iCalendarPlugin from '@fullcalendar/icalendar';
-
 import './fullcalendar.css';
-
 
 const FullCalendarBlockView = (props) => {
   /* server-side rendering with FullCalendar does not work here,
@@ -26,48 +25,63 @@ const FullCalendarBlockView = (props) => {
     } catch (_) {
       return false;
     }
-    return url.protocol === "http:" || url.protocol === "https:";
+    return url.protocol === 'http:' || url.protocol === 'https:';
   }
 
   const { data } = props;
-  const events = (data.calendar_url && isValidURL(data.calendar_url)) ? {
-    url: data.calendar_url,
-    format: 'ics'
-  } : {};
+  const remoteEvents =
+    data.calendar_url && isValidURL(data.calendar_url)
+      ? {
+          url: data.calendar_url,
+          format: 'ics',
+        }
+      : {};
 
-  /* we cannot use a state variable here to show/hide loading spinner
-     since this toggles a re-render,
-     which toggles FullCalendar re-render,
-     which toggles re-load of resource file in FullCalendar
-     -> infinite loop.
-     solution: https://stackoverflow.com/a/67437613
+  const calendarRef = useRef(null);
+
+  /* store events in component state after FullCalendar retrieved them initially,
+     otherwise FullCalendar would reload them on every re-render of this component
   */
-  const loader = useRef(null);
-  const toggleLoader = useCallback((state) => {
-    if (loader.current) {
-      if (state) {
-        loader.current.classList.remove('hidden');
-      }
-      else {
-        loader.current.classList.add('hidden');
-      }
+  const [storedEvents, setStoredEvents] = useState([]);
+  const onLoading = (isLoading) => {
+    if (isLoading === false) {
+      setTimeout(() => {
+        let events = calendarRef.current.getApi().getEvents();
+        if (events.length > 0) {
+          setStoredEvents(events);
+        }
+      });
     }
-  }, [loader]);
+  };
 
-  return isClientSide && (
-    <>
-      <div ref={loader} className="fc-loader-wrapper hidden">
-        <Loader size="massive" />
-      </div>
-      <FullCalendar
-        plugins={[dayGridPlugin, iCalendarPlugin]}
-        initialView="dayGridMonth"
-        events={events}
-        loading={(isLoading) => toggleLoader(isLoading)}
-      />
-    </>
+  return (
+    isClientSide && (
+      <>
+        {storedEvents.length === 0 && (
+          <>
+            <div className="fc-loader-wrapper">
+              <Loader size="massive" />
+            </div>
+            <FullCalendar
+              ref={calendarRef}
+              plugins={[dayGridPlugin, iCalendarPlugin]}
+              initialView="dayGridMonth"
+              events={remoteEvents}
+              loading={(isLoading) => onLoading(isLoading)}
+            />
+          </>
+        )}
+        {storedEvents.length > 0 && (
+          <FullCalendar
+            ref={calendarRef}
+            plugins={[dayGridPlugin, iCalendarPlugin]}
+            initialView="dayGridMonth"
+            events={storedEvents}
+          />
+        )}
+      </>
+    )
   );
 };
 
 export default FullCalendarBlockView;
-
